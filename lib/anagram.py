1b565abd492c3ca4ac006d20c74ab8f2d3c2cd98
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-05-27 14:15:06 +0300

    Implement Anagram class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  
+    def match(self, possible_anagrams):
+        correct_anagrams = []
+
+        for candidate in possible_anagrams:
+            candidate_lower = candidate.lower()
+
+            if candidate_lower != self.word:
+                if sorted(list(candidate_lower)) == sorted(list(self.word)):
+                    correct_anagrams.append(candidate)
+
+        return correct_anagrams
+
+# Example
+if __name__ == "__main__":
+    listen = Anagram("listen")
+    possible_anagrams = ['enlists', 'google', 'inlets', 'banana']
+    result = listen.match(possible_anagrams)
+    print(result)  # Output: ['inlets']
